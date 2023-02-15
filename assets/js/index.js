11a620facc6c61f7fc62bc421be66add6a220304
Dương Văn Hiệp <dhiep2307@gmail.com>
2023-02-16 01:10:24 +0700

    test

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+var typing = new Typed('#typed', {
+    strings: [
+        'Fullstack Developer.',
+        'Eat, Sleep and Code.',
+        'Good Boy.'
+    ],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 8000,
+    loop: true
+});
